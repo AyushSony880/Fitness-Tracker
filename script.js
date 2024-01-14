d0bf8f6097979850bd917f921238e92468cf544b
AyushSony880 <ayushak880@gmail.com>
2024-01-14 19:19:05 +0530

    modified deleting function

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const workOutInputMin = document.getElementById("workOutInputMin");
 const AddBtn = document.getElementById("AddBtn");
 const warning = document.querySelector(".warning");
 
-
 let addedTask = [];
 
 //Adding Task
@@ -25,29 +24,35 @@ AddBtn.addEventListener("click", () => {
     <div id="editDelBtn">
       <button id="edit" >Edit</button>
       <button id="delete">Delete</button>
-    </div>`;
+      </div>`;
+    console.log(addedTask);
     AddListSection.append(AddedEachDiv);
     workOutInputMin.value = "";
     workOutInputTxt.value = "";
-}
+  }
 });
-
 
 // editing And Deletion
 AddListSection.addEventListener("click", (e) => {
-    if (e.target.id === "delete") {
-    e.target.parentElement.parentElement.remove();  // Deleting
-}
-if (e.target.id === "edit") {    // editing
-    const val=e.target.parentElement.parentElement.childNodes[1].innerHTML;
-    const arr = val.split('-');
+  if (e.target.id === "delete") {
+    let CurrentIndex = addedTask.findIndex(
+      (obj) => obj.Duration === workOutInputMin.value
+    );
+    addedTask.splice(addedTask[CurrentIndex], 1);
+    e.target.parentElement.parentElement.remove(); // Deleting
+  }
+  if (e.target.id === "edit") {
+    // editing
+    const val = e.target.parentElement.parentElement.childNodes[1].innerHTML;
+    const arr = val.split("-");
     workOutInputMin.value = parseInt(arr[1]);
     workOutInputTxt.value = arr[0];
-    let CurrentIndex =addedTask.findIndex(obj=>obj.Duration===workOutInputMin.value)
-    addedTask[CurrentIndex].workOutType=workOutInputTxt.value
-    addedTask[CurrentIndex].Duration=workOutInputMin.value;
+    let CurrentIndex = addedTask.findIndex(
+      (obj) => obj.Duration === workOutInputMin.value
+    );
+    addedTask[CurrentIndex].workOutType = workOutInputTxt.value;
+    addedTask[CurrentIndex].Duration = workOutInputMin.value;
     addedTask.splice(addedTask[CurrentIndex], 1);
     e.target.parentElement.parentElement.remove();
   }
 });
-
